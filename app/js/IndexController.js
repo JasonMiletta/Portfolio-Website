587d183bdbbae5a803312ed8cffd6424d96552d2
Jason Miletta <jmiletta01@gmail.com>
2016-04-08 21:16:25 -0500

    Restructured into Visual Studio, Added Angular controllers

---
+++ app/js/IndexController.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+ portfolioApp.controller('IndexController',
+        function ($scope) {
+            
+            $scope.me = {
+                Name : 'Jason Miletta',
+                Title : 'Software Engineer',
+                PictureUrl : '/app/img/JasonMiletta.jpg'
+            }
+        }
+    )
---
+++ app/js/app.js
@@ -0,0 +1,3 @@
+﻿'use strict';
+
+var portfolioApp = angular.module('portfolioApp', []);
