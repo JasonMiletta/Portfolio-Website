5b5bc5c25682e1868d910a6334744bcb036ffabc
Jason Miletta <jmiletta01@gmail.com>
2016-04-12 16:18:20 -0500

    Built out HtmlGame angular Controller and page.

--- app/components/htmlGames/htmlGameController.js
+++ app/components/htmlGames/htmlGameController.js
@@ -2,8 +2,12 @@
 
 portfolioApp.controller('htmlGameController',
        function ($scope) {
-           $scope.game1 = {
-               name : '1'
-           }
+           $scope.games = [
+               { name: 'BlockDodger', fileSrc: 'assets/html5Games/Game1_BlockDodger/index.html', imgSrc: '' },
+               { name: 'Onslaught', fileSrc: 'assets/html5Games/Game2_Onslaught/Onslaught.html', imgSrc: '' },
+               { name: 'ShortCircuit', fileSrc: 'assets/html5Games/Game3_ShortCircuit/Short Circuit.html', imgSrc: '' },
+               { name: 'Knights', fileSrc: 'assets/html5Games/Game4_Knights/Knight.html', imgSrc: '' }
+           ];
+           
        }
    )
