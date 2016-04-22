c9260eda8ba463bdafa432d7ae62aba8b938fae3
Jason Miletta <jmiletta01@gmail.com>
2016-04-21 21:56:36 -0500

    Add thumbnails to HTML5 Games page

--- app/components/htmlGames/htmlGameController.js
+++ app/components/htmlGames/htmlGameController.js
@@ -3,10 +3,26 @@
 portfolioApp.controller('htmlGameController',
        function ($scope) {
            $scope.games = [
-               { name: 'BlockDodger', fileSrc: 'assets/html5Games/Game1_BlockDodger/index.html', imgSrc: '' },
-               { name: 'Onslaught', fileSrc: 'assets/html5Games/Game2_Onslaught/Onslaught.html', imgSrc: '' },
-               { name: 'ShortCircuit', fileSrc: 'assets/html5Games/Game3_ShortCircuit/Short Circuit.html', imgSrc: '' },
-               { name: 'Knights', fileSrc: 'assets/html5Games/Game4_Knights/Knight.html', imgSrc: '' }
+               {
+                   name: 'Block Dodger',
+                   fileSrc: 'assets/html5Games/Game1_BlockDodger/index.html',
+                   imgSrc: 'assets/html5Games/Game1_BlockDodger/assets/Thumbnail.png'
+               },
+               {
+                   name: 'Onslaught',
+                   fileSrc: 'assets/html5Games/Game2_Onslaught/Onslaught.html',
+                   imgSrc: 'assets/html5Games/Game2_Onslaught/assets/Thumbnail.png'
+               },
+               {
+                   name: 'Short Circuit',
+                   fileSrc: 'assets/html5Games/Game3_ShortCircuit/Short Circuit.html',
+                   imgSrc: 'assets/html5Games/Game3_ShortCircuit/assets/Thumbnail.png'
+               },
+               {
+                   name: 'Knights (Under Construction)',
+                   fileSrc: 'assets/html5Games/Game4_Knights/Knight.html',
+                   imgSrc: 'assets/html5Games/Game4_Knights/assets/Thumbnail.png'
+               }
            ];
            
        }
