458952a449a0fc5c90d2722f72f05039a7aaae18
Jason Miletta <jmiletta01@gmail.com>
2016-05-11 17:48:20 -0500

    Add initial iOS portfolio page

---
+++ app/components/iOSPorfolio/iOSController.js
@@ -0,0 +1,13 @@
+portfolioApp.controller('IOSController',
+        function ($scope) {
+            
+            $scope.projects = [
+                {
+                    name : 'Test Project Name',
+                    imgSrc : 'assets/img/iOSThumbnails/test.png',
+                    desc : 'This is a test description'
+                }
+            ];
+            
+        }
+    )
